a58e1372a9e52453262dc49779d6534a0384e07a
Antoine Aflalo <antoine@aaflalo.me>
2019-02-13 11:32:27 -0500

    test(Fields): Improves testing suite for Fields

--- tests/JsonCSV.test.js
+++ tests/JsonCSV.test.js
@@ -92,33 +92,53 @@ describe('Component', () => {
       firstData.toHaveProperty('gender', 'Male')
     })
   })
+  describe('filtering', () => {
+    test('using array', () => {
+      const wrapper = mount(Component, {
+        propsData: {
+          data: [
+            {'id': 1, 'fname': 'Jesse', 'lname': 'Simmons', 'date': '2016-10-15 13:43:27', 'gender': 'Male'},
+            {'id': 2, 'fname': 'John', 'lname': 'Jacobs', 'date': '2016-12-15 06:00:53', 'gender': 'Male'},
+            {'id': 3, 'fname': 'Tina', 'lname': 'Gilbert', 'date': '2016-04-26 06:26:28', 'gender': 'Female'},
+            {'id': 4, 'fname': 'Clarence', 'lname': 'Flores', 'date': '2016-04-10 10:28:46', 'gender': 'Male'},
+            {'id': 5, 'fname': 'Anne', 'lname': 'Lee', 'date': '2016-12-06 14:38:38', 'gender': 'Female'}
+          ],
+          fields: ['fname', 'lname']
+        }
+      })
+      const vm = wrapper.vm
 
-  test('is filtering & labeling the columns', () => {
-    const wrapper = mount(Component, {
-      propsData: {
-        data: [
-          {'id': 1, 'fname': 'Jesse', 'lname': 'Simmons', 'date': '2016-10-15 13:43:27', 'gender': 'Male'},
-          {'id': 2, 'fname': 'John', 'lname': 'Jacobs', 'date': '2016-12-15 06:00:53', 'gender': 'Male'},
-          {'id': 3, 'fname': 'Tina', 'lname': 'Gilbert', 'date': '2016-04-26 06:26:28', 'gender': 'Female'},
-          {'id': 4, 'fname': 'Clarence', 'lname': 'Flores', 'date': '2016-04-10 10:28:46', 'gender': 'Male'},
-          {'id': 5, 'fname': 'Anne', 'lname': 'Lee', 'date': '2016-12-06 14:38:38', 'gender': 'Female'}
-        ],
-        labels: {
-          fname: 'First name',
-          lname: 'Last name'
-        },
-        fields: ['fname', 'lname']
-      }
+      const exportableData = vm.exportableData
+      const firstData = expect(exportableData[0])
+      firstData.toHaveProperty('fname', 'Jesse')
+      firstData.toHaveProperty('lname', 'Simmons')
+      firstData.not.toHaveProperty('id')
+      firstData.not.toHaveProperty('date')
+      firstData.not.toHaveProperty('gender')
     })
-    const vm = wrapper.vm
+    test('using function', () => {
+      const wrapper = mount(Component, {
+        propsData: {
+          data: [
+            {'id': 1, 'fname': 'Jesse', 'lname': 'Simmons', 'date': '2016-10-15 13:43:27', 'gender': 'Male'},
+            {'id': 2, 'fname': 'John', 'lname': 'Jacobs', 'date': '2016-12-15 06:00:53', 'gender': 'Male'},
+            {'id': 3, 'fname': 'Tina', 'lname': 'Gilbert', 'date': '2016-04-26 06:26:28', 'gender': 'Female'},
+            {'id': 4, 'fname': 'Clarence', 'lname': 'Flores', 'date': '2016-04-10 10:28:46', 'gender': 'Male'},
+            {'id': 5, 'fname': 'Anne', 'lname': 'Lee', 'date': '2016-12-06 14:38:38', 'gender': 'Female'}
+          ],
+          fields: (value, key) => _.endsWith(key, 'name')
+        }
+      })
+      const vm = wrapper.vm
 
-    const exportableData = vm.exportableData
-    const firstData = expect(exportableData[0])
-    firstData.toHaveProperty('First name', 'Jesse')
-    firstData.toHaveProperty('Last name', 'Simmons')
-    firstData.not.toHaveProperty('id')
-    firstData.not.toHaveProperty('date')
-    firstData.not.toHaveProperty('gender')
+      const exportableData = vm.exportableData
+      const firstData = expect(exportableData[0])
+      firstData.toHaveProperty('fname', 'Jesse')
+      firstData.toHaveProperty('lname', 'Simmons')
+      firstData.not.toHaveProperty('id')
+      firstData.not.toHaveProperty('date')
+      firstData.not.toHaveProperty('gender')
+    })
   })
 
   test('is triggering event on export', () => {
