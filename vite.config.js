695877b3fad892dc0009431d494744eb727cebc6
Andrew Donald Johnson <ajohnson6494@gmail.com>
2022-03-08 21:26:35 -0500

    improvement(Vue3): Upgrade to Vue 3

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+// import { resolve } from 'path'
+
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    open: true
+  },
+  root: 'example',
+  base: './',
+  publicDir: 'docs'
+})
